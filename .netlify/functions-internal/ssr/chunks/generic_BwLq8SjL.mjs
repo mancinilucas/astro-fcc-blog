be86f63eb63d81ce69a15856a847c0161e0084fd
mancinilucas <mancini.lucasg@gmail.com>
2024-02-24 18:48:13 -0300

    fixed build

--- .netlify/functions-internal/ssr/chunks/generic_BwLq8SjL.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_Bn3EoBnh.mjs');
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/the-importance-of-audio-quality_DxbZrmm3.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./the-importance-of-audio-quality_BtYFtC4H.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
